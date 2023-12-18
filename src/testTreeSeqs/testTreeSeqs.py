2e0af1a3e4557140199a6dd957a942ffb9e5549b
Hannes Becher <hannesbecher@gmx.de>
2023-12-18 11:28:44 +0000

    arg parser

---
+++ src/testTreeSeqs/testTreeSeqs.py
@@ -0,0 +1,29 @@
+import argparse
+import msprime
+
+parser = argparse.ArgumentParser(
+                    prog='testTreeSeqs',
+                    description='A package to generate test data for the HLab TS pipeline. Based on msprime.',
+                    epilog='Thanks for using testTreeSeqs!')
+
+
+parser.add_argument("-s", "--seed") # random seed
+
+
+
+
+def makeSim():
+    ts = msprime.sim_ancestry(
+        samples=10,
+        recombination_rate=9.26e-09,
+        sequence_length=50_000,
+        population_size=1_000,
+        random_seed=123456)
+
+    ts = msprime.sim_mutations(
+        ts,
+        rate=1.2e-08,
+        random_seed=54321
+        )
+    
+    return ts
